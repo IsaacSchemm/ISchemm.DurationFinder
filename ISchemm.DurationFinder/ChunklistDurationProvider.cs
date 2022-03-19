3df41a47e489ff9915a4a17716f78118010514c9
Isaac Schemm <isaacschemm@gmail.com>
2022-03-19 17:01:17 -0500

    Split internally used providers into new files

--- ISchemm.DurationFinder/ChunklistDurationProvider.cs
+++ ISchemm.DurationFinder/ChunklistDurationProvider.cs
@@ -4,9 +4,7 @@ using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace ISchemm.DurationFinder {
-    public class HlsDurationProvider : IDurationProvider {
-        private readonly IDurationProvider _chunklistProvider = new ChunklistDurationProvider();
-
+    public class ChunklistDurationProvider : IDurationProvider {
         public async Task<TimeSpan?> GetDurationAsync(Uri originalLocation, HttpContent httpContent) {
             if (!httpContent.IsOfType("application/x-mpegURL", "application/x-vnd.apple.mpegURL", "audio/mpegURL", "audio/x-mpegURL"))
                 return null;
@@ -16,12 +14,14 @@ namespace ISchemm.DurationFinder {
             string body = await httpContent.ReadAsStringAsync();
             using var sr = new StringReader(body);
 
-            string line;
+            TimeSpan ts = TimeSpan.Zero;
+
+            string line = "";
             while ((line = await sr.ReadLineAsync()) != null)
-                if (!line.StartsWith("#") && line.EndsWith(".m3u8", StringComparison.OrdinalIgnoreCase))
-                    if (Uri.TryCreate(originalLocation, line, out Uri chunklist))
-                        if (await _chunklistProvider.GetDurationAsync(chunklist) is TimeSpan ts)
-                            return ts;
+                if (line.StartsWith("#EXTINF:"))
+                    ts += TimeSpan.FromSeconds(double.Parse(line.Substring("#EXTINF:".Length).Split(',')[0]));
+                else if (line == "#EXT-X-ENDLIST")
+                    return ts;
 
             return null;
         }
